fb5d676af53bee4e4f085490bbab326832b01644
spiros3p <spiros3p@gmail.com>
2023-11-29 00:59:21 +0200

    add primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    // BrowserModule,
+    // BrowserAnimationsModule
+  ],
   selector: 'flovatar-dust-bank-app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
